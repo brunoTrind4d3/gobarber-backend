9dc9180a13eedd25ad851fe0eb3f184d12161e56
Bruno Trindade <b.l.trindade@outlook.com>
2020-02-04 13:01:23 -0300

    Criptografia de senha informada pelo usuario

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Bruno Trindade',
-    email: 'b.l.trindade@outlook.com',
-    password_hash: 'q38482378',
-  });
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
+
 export default routes;
