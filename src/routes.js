152b01b842aaa22adaa41c27de45c9a0510298f4
Bruno Trindade <b.l.trindade@outlook.com>
2020-02-04 00:36:27 -0300

    Cria model de usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,13 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
-
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Bruno Trindade',
+    email: 'b.l.trindade@outlook.com',
+    password_hash: 'q38482378',
+  });
+  return res.json(user);
+});
 export default routes;
