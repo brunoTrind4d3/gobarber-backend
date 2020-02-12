a314d6aa56ce2217f6e2912e65d86d617acc8e96
Bruno Trindade <b.l.trindade@outlook.com>
2020-02-12 00:14:50 -0300

    Appointments list and MongoDB config

--- src/routes.js
+++ src/routes.js
@@ -7,6 +7,7 @@ import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
 import authMiddleware from './app/middlewares/auth';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -19,6 +20,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.get('/schedule', ScheduleController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
