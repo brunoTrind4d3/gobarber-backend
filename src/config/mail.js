4cb3fee1d719bb50fc7344822f0e4fee204794a8
Bruno Trindade <b.l.trindade@outlook.com>
2020-02-15 11:22:31 -0300

    Send mail to providers

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '4886c1d279100b',
+    pass: 'fb4996027e1b46',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
